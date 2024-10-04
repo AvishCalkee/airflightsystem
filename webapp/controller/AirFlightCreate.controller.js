d702b9bfc32ac77408d7c7986db3ea124282b679
GirishLuchmun <girish.luchmun@accenture.com>
2024-10-04 14:28:39 +0400

    Mise a jour écran de création

--- webapp/controller/AirFlightCreate.controller.js
+++ webapp/controller/AirFlightCreate.controller.js
@@ -7,6 +7,42 @@ function (Controller) {
     return Controller.extend("fiori.bootcamp.airflightsystem.controller.AirFlightCreate", {
         onInit: function () {
             
-        }
+        },
+		/*handle date change - formatting*/
+		onHandleDateChange: function (oEvent) {
+			
+		},
+		/*Airline valuehelp*/
+		onRequestAirline: function () {
+			
+		},
+		/*Flight valuehelp*/
+		onRequestFlight: function () {
+			
+		},
+		/*Flight Number valuehelp*/
+		onFlightNumber: function () {
+			
+		},
+		/*Airport code valuehelp*/
+		onRequestAirportCode: function () {
+			
+		},
+		/*Seat valuehelp*/
+		onRequestSeat: function() {
+			
+		},
+		/*Passenger valuehelp*/
+		onRequestPassenger: function() {
+			
+		},
+		/*Save Booking*/
+		onPressSaveBooking: function (oEvent) {
+			
+		},
+		/*Cancel Booking - clear booking screen*/
+		onPressCancelBooking: function (oEvent) {
+			
+		}
     });
 });
